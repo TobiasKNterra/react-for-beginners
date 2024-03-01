0dab3cf9db1aa0db82085118e1169df42c2a4ac2
Tobias Kaiser <158767669+TobiasKNterra@users.noreply.github.com>
2024-03-01 14:33:34 +0100

    update main container

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -3,11 +3,11 @@ import reactSVG from "./assets/react.svg";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h2 style={{ color: "black" }}>Welcome to</h2>
       <h1 style={{ color: "black" }}>React for Beginners</h1>
       <img src={reactSVG}></img>
-    </>
+    </div>
   );
 }
 
