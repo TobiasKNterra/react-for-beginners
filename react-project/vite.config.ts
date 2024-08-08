09cd70817ce09658db5c9beaeb9586712247486d
Tobias Kaiser <tobias.kaiser@nterra.com>
2024-08-08 11:39:52 +0200

    adds lint checker

--- react-project/vite.config.ts
+++ react-project/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), checker({
+    typescript: true,
+    eslint: {
+      lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+    },
+  }),],
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
